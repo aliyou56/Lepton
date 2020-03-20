affcd0996a757c4d14c6cc9cd459e8bf33892fa6
Malléjac Clément <clementmallejac@gmail.com>
2020-03-20 19:30:02 +0100

    Remove useless comment

--- scenario/scripts/dgs/hist_from_node_file.py
+++ scenario/scripts/dgs/hist_from_node_file.py
@@ -1,16 +1,3 @@
- 
-# first step          : 1000
-# last step           : 3600000
-# duration            : 3599000
-# nb nodes            : 100
-# max concurrent nodes: 100
-#
-# begin end duration node
-# 1000 3600000 3599000 N047
-# 1000 3600000 3599000 N033
-# 1000 3600000 3599000 N008
-# 1000 3600000 3599000 N015
-
 import sys
 
 start = 1000 # Start is assumed to be 1000
