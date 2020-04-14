ffb95a33614b60b00d7d6e21837afa2006491492
Aliyou Sylla <aliyou29@hotmail.com>
2020-04-14 16:27:11 +0200

    Change lepton params handling for ADTN

--- tools/bin/run_adtn.sh
+++ tools/bin/run_adtn.sh
@@ -7,76 +7,62 @@
 ###     A directory scenario_name must exist under ${LEPTON_HOME}/scenario/
 ###       
 
+# usage check
 if [ $# -ne 1 ]; then
     echo "usage: $0 <sceanrio_name>"
+    echo "  A directory 'scenario_name' must exist under \${LEPTON_HOME}/scenario/"
     exit 1
 fi
 
-# check if needed global variables are set
-if [ -z $LEPTON_HOME ]; then
-    echo "$0 -> Error: \$LEPTON_HOME is not defined."
-	exit 1
-fi
-if [ -z $ADTNPLUS_ADAPTER_HOME ]; then
-    echo "$0 -> Error: \$ADTNPLUS_ADAPTER_HOME is not defined."
-    exit 1
-fi
-
-main_dir=${LEPTON_HOME%/*}
-echo "main_dir -> ${main_dir}"
-
-# clean output directories
+# parent directory for output
+main_dir=${LEPTON_HOME}
+# echo "main_dir        -> ${main_dir}"
+# clean output 
 rm -r ${main_dir}/output/adtn/* 2> /dev/null
 
 # init port number management  
 port_dir="/run/shm/tmp"
 mkdir -p ${port_dir}
 rm -f ${port_dir}/port_aux.lock
-echo "40000" > ${port_dir}/port_aux # 40000 -> starting port number for adtn nodes
+echo "38000" > ${port_dir}/port_aux # 38000 -> starting port number for adtn nodes
 
 
-scenario_to_run=$1 # "levy_walk"
+scenario=$1
+scenario_dir=${LEPTON_HOME}/scenario/${scenario}
+conf_file="${scenario_dir}/lepton.conf"
+echo "scenario -> ${scenario}"    
 
-# if [ $# -ne 0 ]; then
-#     scenario_to_run=$1
-# else
-#     echo " No given scenario to run (\$# = 0), Using DEFAULT scenario"    
-# fi
-echo "scenario_to_run -> ${scenario_to_run}"    
-
-scenario_to_run_dir="${LEPTON_HOME}/scenario/${scenario_to_run}"
-if [ ! -d $scenario_to_run_dir ]; then
-    echo " Error: can't find scenario dir -> ${scenario_to_run_dir}"
+if [ ! -d $scenario_dir ]; then
+    echo " Error: can't find scenario_dir -> ${scenario_dir}"
     exit 2
 fi
 
-conf_file="${scenario_to_run_dir}/lepton.conf"
-if [ ! -f $conf_file ]; then
-    # gen confi
-    
+lepton_params=""
+
+if [ ! -f $conf_file ]; then # if the configuration file not found
+    show=true
+    dgs=${scenario_dir}/${scenario}.dgs
+    hist=${scenario_dir}/${scenario}.hist
+    time_margin=10 
+    make_edges=true
+
+    lepton_params+="in_dgs=$dgs in_hist=$hist time_margin=$time_margin make_edges=$make_edges show=$show"
+else
+    lepton_params+="conf=${conf_file}"    
 fi
-echo "conf_file -> ${conf_file}" 
 
-show=true
-margin=10
-dgs=${scenario_to_run_dir}/${scenario_to_run}.dgs
-hist=${scenario_to_run_dir}/${scenario_to_run}.hist
-css=${scenario_to_run_dir}/${scenario_to_run}.css
+lepton_params+=" oppnet_adapter=${ADTNPLUS_ADAPTER_HOME}/bin/adapter.sh"
+echo "lepton_params   -> $lepton_params"
+echo ""
 
-# start lepton with the defined configuration file
-lepton.sh start \
-		show=$show \
-		time_margin=$margin \
-		in_dgs=$dgs \
-		in_hist=$hist \
-		stylesheet_file=$css \
-        oppnet_adapter=${ADTNPLUS_ADAPTER_HOME}/bin/adapter.sh
+# start lepton with the defined params
+lepton.sh start $lepton_params
         
-sleep 5s # 
-in_aevt=${scenario_to_run_dir}/${scenario_to_run}.aevt
-out_aevt=${scenario_to_run_dir}/${scenario_to_run}_out_adtn.aevt
+sleep 11s # 
+in_aevt=${scenario_dir}/${scenario}.aevt
+out_aevt=${scenario_dir}/${scenario}_out_adtn.aevt
 
-# runnig app scenario
+# # runnig app scenario
 ./util/run_app_scenario_adtn.sh $in_aevt #&
 
 # sleep 1510s # waiting for the end of the simulation
