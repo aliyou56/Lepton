049d021b31aeb2163ac9a90d74b04bd646c394f1
Aliyou Sylla <aliyou29@hotmail.com>
2020-05-01 05:46:08 +0200

    Fix Log analyzer script launcher

--- tools/bin/util/analyze_log_adtn.sh
+++ tools/bin/util/analyze_log_adtn.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-if [ -z main_dir ]; then
-    main_dir={LEPTON_HOME%/*}
+script_dir=$(realpath $(dirname $0))
+
+if [ -z ${main_dir} ]; then
+    main_dir={LEPTON_HOME}
+    if [[ "${LEPTON_VAR}" != "" ]]; then
+        current_dir=$(pwd)
+        cd ${LEPTON_VAR}/../.. 
+        main_dir=$(pwd)
+        cd $current_dir
+    fi
+fi
+
+if [ $# -ne 1 ] ; then
+    echo "usage: $0 <output-file>"
+    exit 1
 fi
 
 lepton_out_file=${main_dir}/output/lepton/lepton.out
 adtn_out_dir=${main_dir}/output/adtn
-# $1 -> in_aevt
-# $2 -> out_aevt
-scala ./util/libs/adtnLogAnalyzer.jar ${lepton_out_file} ${adtn_out_dir} $1 $2 
+
+if [ ! -f ${lepton_out_file} ]; then 
+    echo "[analyze_log_adtn]: File not found -> ${lepton_out_file}"
+    exit 1
+fi
+if [ ! -d ${adtn_out_dir} ]; then 
+    echo "[analyze_log_adtn]: Directory not found -> ${adtn_out_dir}"
+    exit 1
+fi
+
+scala ${script_dir}/libs/adtnLogAnalyzer.jar ${lepton_out_file} ${adtn_out_dir} $1
